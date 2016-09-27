ca996b820aa84a65ebf4aaefe8800a9611f2a8fa
k-sathireddy <k.sathireddy314@gmail.com>
2016-09-27 18:53:21 +0530

    UitableView code added..

--- TableViewSelectedCheckMarks/ViewController.m
+++ TableViewSelectedCheckMarks/ViewController.m
@@ -8,8 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *languagesTableView;
 
+@property (strong, nonatomic) NSArray *languagesArray;
+
+@property (strong, nonatomic) NSMutableArray *checkMarksArray;
 @end
 
 @implementation ViewController
@@ -17,6 +21,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.languagesTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    self.languagesArray = [NSArray arrayWithObjects:@"English",@"Spanish",@"Russian",@"Arabic",@"Portuguese",@"French",@"German", nil];
+    self.checkMarksArray = [[NSMutableArray alloc]init];
+    if( [[NSUserDefaults standardUserDefaults]objectForKey:@"selectedRowsArray"])
+    {
+       self.checkMarksArray = [[[NSUserDefaults standardUserDefaults]objectForKey:@"selectedRowsArray"] mutableCopy];
+    }
 }
 
 
@@ -25,5 +37,48 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewWillDisappear:(BOOL)animated{
+   
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.languagesArray.count;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    UITableViewCell *languagesCell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    languagesCell.textLabel.text = self.languagesArray[indexPath.row];
+    if([self.checkMarksArray containsObject:[NSNumber numberWithLong:indexPath.row]])
+    {
+        languagesCell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else
+    {
+        languagesCell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [[NSUserDefaults standardUserDefaults]setObject:self.checkMarksArray forKey:@"selectedRowsArray"];
+    [[NSUserDefaults standardUserDefaults]synchronize];
+    
+    return languagesCell;
+    
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    if([self.checkMarksArray containsObject:[NSNumber numberWithLong:indexPath.row]])
+    {
+        [self.checkMarksArray removeObject:[NSNumber numberWithLong:indexPath.row]];
+    }
+    else
+    {
+        [self.checkMarksArray addObject:[NSNumber numberWithLong:indexPath.row]];
+    }
+    
+    [self.languagesTableView reloadData];
+}
 
 @end
