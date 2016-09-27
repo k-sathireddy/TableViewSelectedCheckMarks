c7b0f284a657c2610c53a32a2b85018176a209d4
k-sathireddy <k.sathireddy314@gmail.com>
2016-09-27 18:22:56 +0530

    Initial Commit

---
+++ TableViewSelectedCheckMarks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewSelectedCheckMarks
+//
+//  Created by Enterpi mini mac on 9/27/16.
+//  Copyright © 2016 Enterpi mini mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewSelectedCheckMarks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewSelectedCheckMarks
+//
+//  Created by Enterpi mini mac on 9/27/16.
+//  Copyright © 2016 Enterpi mini mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
